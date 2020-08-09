855496c3dd7c89cf1a85c25bf3e1755502a4cf96
unknown <yugang.wu@qtech.com>
2020-08-09 19:43:26 +0800

    day05

--- 03.The Lifecycle of a Plot/sample01.py
+++ 03.The Lifecycle of a Plot/sample01.py
@@ -2,6 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
+
+def currency(x, pos):
+    """The two args are the value and tick position"""
+    if x >= 1e6:
+        s = '${:1.1f}M'.format(x*1e-6)
+    else:
+        s = '${:1.0f}K'.format(x*1e-3)
+    return s
+
+
+formatter = FuncFormatter(currency)
+
 # 数据
 data = {
     'Barton LLC': 109438.50,
@@ -36,6 +48,8 @@ print(plt.style.available)
 # 使用样式
 plt.style.use('fivethirtyeight')
 
+plt.rcParams.update({'figure.autolayout': True})
+
 # 创建Figure对象
 # 返回值 fig是一个Figure对象，ax是Axes对象的数组，这里我们只生成1个Axes。
 # 可通过nrows= ncols= 来控制生成的Axes的个数，默认情况下nrows=1, ncols=1。因此只生成1个Axes。
@@ -43,6 +57,17 @@ fig,ax = plt.subplots(num="barh")
 
 # 绘制条形图
 ax.barh(group_names,group_data)
+
+# 获取x轴的labels
+labels = ax.get_xticklabels()
+# 设置labels的属性，旋转45度 label右端与tick对齐
+plt.setp(labels, rotation=45,horizontalalignment="right")
+
+# 使用OO方式进行设置轴的属性
+# 设置x轴显示的范围，x轴的label，y轴的label，title.
+ax.set(xlim=[-10000,140000],xlabel="Tolal Revenue",ylabel="Company", title='Company Revenue')
+
+ax.xaxis.set_major_formatter(formatter)
 
 
 
