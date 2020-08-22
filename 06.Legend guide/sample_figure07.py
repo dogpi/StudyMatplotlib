738ad3df9f3c7bfcb4d4da82add49055eeed0e37
unknown <yugang.wu@qtech.com>
2020-08-22 19:11:37 +0800

    2020-8-22

---
+++ 06.Legend guide/sample_figure07.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.legend_handler import HandlerLine2D,HandlerTuple
+
+p1, = plt.plot([1,2.5,3],"r-d")
+p2, = plt.plot([3,2,1],'k-o')
+
+l = plt.legend([(p1,p2)],["Two keys"],numpoints=1,handler_map={tuple:HandlerTuple(ndivide=None)})
+
+plt.show()
